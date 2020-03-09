ce0d7e40c7e99d2f7146c9a840caa9251be0b366
xisigui <andreiulupon@gmail.com>
2020-03-09 22:25:39 +0800

    Add files via upload

--- inventory_project/inventory_project/Form1.cs
+++ inventory_project/inventory_project/Form1.cs
@@ -13,11 +13,9 @@ namespace inventory_project
 {
     public partial class Form1 : Form
     {
-
         public static string connectionString = @"Data Source=DESKTOP-U1UA2TS;Initial Catalog=Inventory_DB;Integrated Security=true";
         public static SqlConnection cnn = new SqlConnection(connectionString);
         public int userID = -1;
-
         public int checkUser(string username, string password){
             int numVal = -1;
             cnn.Open();
@@ -46,12 +44,6 @@ namespace inventory_project
 
             StartPosition = FormStartPosition.CenterScreen;
         }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void btn_exit_Click(object sender, EventArgs e)
         {
             DialogResult dialogResult = MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo);
@@ -59,16 +51,12 @@ namespace inventory_project
             {
                 this.Close();
             }
-            else if (dialogResult == DialogResult.No)
-            {
-            }
         }
 
         private void btn_login_Click(object sender, EventArgs e)
         {
             String username = txtUsername.Text;
             String password = txtPassword.Text;
-
             if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
             {
                 MessageBox.Show("Invalid username or password");
@@ -76,7 +64,6 @@ namespace inventory_project
             else
             {
                 userID = checkUser(username, password);
-
                 if(userID < 0){
                     MessageBox.Show("Invalid username or password", "Invalid");
                 }
